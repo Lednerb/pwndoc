b212f67ef06784408dc14fe75bab5723768b8910
DGC-Mehmet <mehmet.ersoy@dg-cybersicherheit.de>
2021-04-07 12:57:10 +0200

    +small change

--- frontend/src/pages/audits/edit/index.vue
+++ frontend/src/pages/audits/edit/index.vue
@@ -126,71 +126,70 @@
 
             <q-separator class="q-mt-lg" />
 
-					<q-item class="q-py-lg">
-						<q-item-section>Custom Sections</q-item-section>
-						<q-item-section avatar>
-							<q-btn
-							round
-							dense
-							icon="add"
-							color="secondary"
-							>
-								<q-menu v-if="sections.length === 0" anchor="top right" self="top left">
-									<q-item v-close-popup>
-										<q-item-section>No custom sections defined yet</q-item-section>
-									</q-item>
-								</q-menu>
-								<q-menu v-else anchor="top right" self="top left">
-									<q-list separator>
-										<q-item clickable v-close-popup v-for="section of sections" :key="section.field" @click="createSection(section)">
-											<q-item-section>{{section.name}}</q-item-section>
-										</q-item>
-									</q-list>
-								</q-menu>
-							</q-btn>
-						</q-item-section>
-					</q-item>
-					<q-list v-for="section of audit.sections" :key="section._id">
-						<q-item
-						:to="'/audits/'+auditId+'/sections/'+section._id"
-						>
-							<q-item-section avatar>
-								<q-icon :name="getSectionIcon(section)"></q-icon>
-							</q-item-section>
-							<q-item-section>
-								<span>{{section.name}}</span>
-							</q-item-section>
-						</q-item>
-						<div class="row">
-							<div v-for="(user,idx) in sectionUsers" :key="idx" v-if="user.section === section._id" class="col multi-colors-bar" :style="{background:user.color}" />
-						</div>
-
-						<q-separator />
-					</q-list>
-				</q-list>
-			</template>
-			<template v-slot:after>
-				<q-list>
-					<q-separator />
-					<q-item class="q-py-lg">
-						<q-item-section avatar>
-							<q-icon name="fa fa-user"></q-icon>
-						</q-item-section>
-						<q-item-section>Users Connected</q-item-section>
-					</q-item>
-					<q-list dense>
-						<q-item v-for="user of users" :key="user._id">
-							<q-item-section side>
-								<q-chip :style="{'background-color':user.color}" square size="sm" />
-							</q-item-section>
-							<q-item-section>
-								<span v-if="user.me">{{user.username}} (me)</span>
-								<span v-else>{{user.username}}</span>
-							</q-item-section>
-						</q-item>
-					</q-list>
-				</q-list>
-			</template>
+            <q-item class="q-py-lg">
+              <q-item-section>Custom Sections</q-item-section>
+              <q-item-section avatar>
+                <q-btn
+                    round
+                    dense
+                    icon="add"
+                    color="secondary"
+                >
+                  <q-menu v-if="sections.length === 0" anchor="top right" self="top left">
+                    <q-item v-close-popup>
+                      <q-item-section>No custom sections defined yet</q-item-section>
+                    </q-item>
+                  </q-menu>
+                  <q-menu v-else anchor="top right" self="top left">
+                    <q-list separator>
+                      <q-item clickable v-close-popup v-for="section of sections" :key="section.field" @click="createSection(section)">
+                        <q-item-section>{{section.name}}</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-btn>
+              </q-item-section>
+            </q-item>
+            <q-list v-for="section of audit.sections" :key="section._id">
+              <q-item
+                  :to="'/audits/'+auditId+'/sections/'+section._id"
+              >
+                <q-item-section avatar>
+                  <q-icon :name="getSectionIcon(section)"></q-icon>
+                </q-item-section>
+                <q-item-section>
+                  <span>{{section.name}}</span>
+                </q-item-section>
+              </q-item>
+              <div class="row">
+                <div v-for="(user,idx) in sectionUsers" :key="idx" v-if="user.section === section._id" class="col multi-colors-bar" :style="{background:user.color}" />
+              </div>
+              <q-separator />
+            </q-list>
+          </q-list>
+        </template>
+        <template v-slot:after>
+          <q-list>
+            <q-separator />
+            <q-item class="q-py-lg">
+              <q-item-section avatar>
+                <q-icon name="fa fa-user"></q-icon>
+              </q-item-section>
+              <q-item-section>Users Connected</q-item-section>
+            </q-item>
+            <q-list dense>
+              <q-item v-for="user of users" :key="user._id">
+                <q-item-section side>
+                  <q-chip :style="{'background-color':user.color}" square size="sm" />
+                </q-item-section>
+                <q-item-section>
+                  <span v-if="user.me">{{user.username}} (me)</span>
+                  <span v-else>{{user.username}}</span>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-list>
+        </template>
 
       </q-splitter>
     </q-drawer>
@@ -200,208 +199,190 @@
 
 <script>
 import { Notify } from 'quasar';
-
 import AuditService from '@/services/audit';
 import UserService from '@/services/user';
 import DataService from '@/services/data';
-
 export default {
-		data () {
-				return {
-						auditId: "",
-						findings: [],
-						users: [],
-						audit: {findings: {}},
-						sections: [],
-						splitterRatio: 80,
-						loading: true,
-						vulnCategories: [],
-            scopeArrayHeader: [],
-            ownArray:[]
-				}
-		},
-
-		created: function() {
-			this.auditId = this.$route.params.auditId;
-			this.getAudit(); // Calls getSections
-		},
-
-		destroyed: function() {
-			if (!this.loading) {
-				this.$socket.emit('leave', {username: UserService.user.username, room: this.auditId});
-				this.$socket.off()
-			}
-		},
-
-		computed: {
-			generalUsers: function() {return this.users.filter(user => user.menu === 'general')},
-			networkUsers: function() {return this.users.filter(user => user.menu === 'network')},
-			findingUsers: function() {return this.users.filter(user => user.menu === 'editFinding')},
-			sectionUsers: function() {return this.users.filter(user => user.menu === 'editSection')},
-
-			findingList: function() { // Group findings by category
-				return _.chain(this.audit.findings)
-				.groupBy("category")
-				.map((value, key) => {
-					if (key === 'undefined')
-						return { category: 'No Category', findings: value }
-					else
-						return {category: key, findings: value}
-				})
-				.value()
-			},
-      test: function () {
-        this.findingList.map(obj => {
-          obj.findings.map(t => {
-            if(t.scopeArray.length == 0)
-              this.scopeArrayHeader.push(["No Category"])
+  data () {
+    return {
+      auditId: "",
+      findings: [],
+      users: [],
+      audit: {findings: {}},
+      sections: [],
+      splitterRatio: 80,
+      loading: true,
+      vulnCategories: [],
+      scopeArrayHeader: [],
+      ownArray:[]
+    }
+  },
+  created: function() {
+    this.auditId = this.$route.params.auditId;
+    this.getAudit(); // Calls getSections
+  },
+  destroyed: function() {
+    if (!this.loading) {
+      this.$socket.emit('leave', {username: UserService.user.username, room: this.auditId});
+      this.$socket.off()
+    }
+  },
+  computed: {
+    generalUsers: function() {return this.users.filter(user => user.menu === 'general')},
+    networkUsers: function() {return this.users.filter(user => user.menu === 'network')},
+    findingUsers: function() {return this.users.filter(user => user.menu === 'editFinding')},
+    sectionUsers: function() {return this.users.filter(user => user.menu === 'editSection')},
+    findingList: function() { // Group findings by category
+      return _.chain(this.audit.findings)
+          .groupBy("category")
+          .map((value, key) => {
+            if (key === 'undefined')
+              return { category: 'No Category', findings: value }
             else
-              this.scopeArrayHeader.push(t.scopeArray)
-
-          });
+              return {category: key, findings: value}
+          })
+          .value()
+    },
+    test: function () {
+      this.findingList.map(obj => {
+        obj.findings.map(t => {
+          if(t.scopeArray.length == 0)
+            this.scopeArrayHeader.push(["No Category"])
+          else
+            this.scopeArrayHeader.push(t.scopeArray)
         });
-      },
-      filter: function () {
-        const arr = [].concat(...this.scopeArrayHeader)
-        const a = arr.filter((el,i) => arr.indexOf(el) === i)
-        return a
-      },
-		},
-
-		methods: {
-			getFindingColor: function(finding) {
-				if (finding.cvssSeverity && finding.cvssSeverity !== "None") {
-					if (finding.cvssSeverity === "Low") return "green"
-					if (finding.cvssSeverity === "Medium") return "orange"
-					if (finding.cvssSeverity === "High") return "red"
-					if (finding.cvssSeverity === "Critical") return "black"
-				}
-				else if (finding.priority) {
-					if (finding.priority === 1) return "green"
-					if (finding.priority === 2) return "orange"
-					if (finding.priority === 3) return "red"
-					if (finding.priority === 4) return "black"
-				}
-				return "light-blue";
-			},
-
-			// Sockets handle
-			handleSocket: function() {
-				this.$socket.emit('join', {username: UserService.user.username, room: this.auditId});
-				this.$socket.on('roomUsers', (users) => {
-					var userIndex = 0;
-					this.users = users.map((user,index) => {
-						if (user.username === UserService.user.username) {
-							user.color = "#77C84E";
-							user.me = true;
-							userIndex = index;
-						}
-						return user;
-					});
-					this.users.unshift(this.users.splice(userIndex, 1)[0]);
-				})
-				this.$socket.on('updateUsers', () => {
-					this.$socket.emit('updateUsers', {room: this.auditId})
-				})
-				this.$socket.on('updateAudit', () => {
-					this.getAudit();
-				})
-			},
-
-			getAudit: function() {
-				AuditService.getAudit(this.auditId)
-				.then((data) => {
-					this.audit = data.data.datas
-					this.getSections()
-					if (this.loading)
-						this.handleSocket()
-					this.loading = false
-				})
-				.catch((err) => {
-					if (err.response.status === 403)
-						this.$router.push({name: '403', params: {error: err.response.data.datas}})
-					else if (err.response.status === 404)
-						this.$router.push({name: '404', params: {error: err.response.data.datas}})
-				})
-			},
-
-			getSections: function() {
-				DataService.getSectionsByLanguage(this.audit.language)
-				.then((data) => {
-					this.sections = data.data.datas;
-				})
-				.catch((err) => {
-					console.log(err);
-				})
-			},
-
-			getSectionIcon: function(section) {
-				var section = this.sections.find(e => e.field === section.field)
-				if (section)
-					return section.icon || 'notes'
-				return 'notes'
-			},
-
-			createSection: function(section) {
-				AuditService.createSection(this.auditId, section)
-				.catch((err) => {
-					Notify.create({
-						message: err.response.data.datas,
-						color: 'negative',
-						textColor: 'white',
-						position: 'top-right'
-					})
-				})
-			},
-
-			// Convert blob to text
-        BlobReader: function(data) {
-            const fileReader = new FileReader();
-
-            return new Promise((resolve, reject) => {
-                fileReader.onerror = () => {
-                    fileReader.abort()
-                    reject(new Error('Problem parsing blob'));
-                }
-
-                fileReader.onload = () => {
-                    resolve(fileReader.result)
-                }
-
-                fileReader.readAsText(data)
+      });
+    },
+    filter: function () {
+      const arr = [].concat(...this.scopeArrayHeader)
+      const a = arr.filter((el,i) => arr.indexOf(el) === i)
+      return a
+    },
+  },
+  methods: {
+    getFindingColor: function(finding) {
+      if (finding.cvssSeverity && finding.cvssSeverity !== "None") {
+        if (finding.cvssSeverity === "Low") return "green"
+        if (finding.cvssSeverity === "Medium") return "orange"
+        if (finding.cvssSeverity === "High") return "red"
+        if (finding.cvssSeverity === "Critical") return "black"
+      }
+      else if (finding.priority) {
+        if (finding.priority === 1) return "green"
+        if (finding.priority === 2) return "orange"
+        if (finding.priority === 3) return "red"
+        if (finding.priority === 4) return "black"
+      }
+      return "light-blue";
+    },
+    // Sockets handle
+    handleSocket: function() {
+      this.$socket.emit('join', {username: UserService.user.username, room: this.auditId});
+      this.$socket.on('roomUsers', (users) => {
+        var userIndex = 0;
+        this.users = users.map((user,index) => {
+          if (user.username === UserService.user.username) {
+            user.color = "#77C84E";
+            user.me = true;
+            userIndex = index;
+          }
+          return user;
+        });
+        this.users.unshift(this.users.splice(userIndex, 1)[0]);
+      })
+      this.$socket.on('updateUsers', () => {
+        this.$socket.emit('updateUsers', {room: this.auditId})
+      })
+      this.$socket.on('updateAudit', () => {
+        this.getAudit();
+      })
+    },
+    getAudit: function() {
+      AuditService.getAudit(this.auditId)
+          .then((data) => {
+            this.audit = data.data.datas
+            this.getSections()
+            if (this.loading)
+              this.handleSocket()
+            this.loading = false
+          })
+          .catch((err) => {
+            if (err.response.status === 403)
+              this.$router.push({name: '403', params: {error: err.response.data.datas}})
+            else if (err.response.status === 404)
+              this.$router.push({name: '404', params: {error: err.response.data.datas}})
+          })
+    },
+    getSections: function() {
+      DataService.getSectionsByLanguage(this.audit.language)
+          .then((data) => {
+            this.sections = data.data.datas;
+          })
+          .catch((err) => {
+            console.log(err);
+          })
+    },
+    getSectionIcon: function(section) {
+      var section = this.sections.find(e => e.field === section.field)
+      if (section)
+        return section.icon || 'notes'
+      return 'notes'
+    },
+    createSection: function(section) {
+      AuditService.createSection(this.auditId, section)
+          .catch((err) => {
+            Notify.create({
+              message: err.response.data.datas,
+              color: 'negative',
+              textColor: 'white',
+              position: 'top-right'
             })
-        },
-
-        generateReport: function() {
-            AuditService.generateAuditReport(this.auditId)
-            .then(response => {
-                var blob = new Blob([response.data], {type: "application/octet-stream"});
-                var link = document.createElement('a');
-                link.href = window.URL.createObjectURL(blob);
-                link.download = response.headers['content-disposition'].split('"')[1];
-                document.body.appendChild(link);
-                link.click();
-                link.remove();
-              })
-            .catch( async err => {
-                var message = "Error generating template"
-                if (err.response && err.response.data) {
-                    var blob = new Blob([err.response.data], {type: "application/json"})
-                    var blobData = await this.BlobReader(blob)
-                    message = JSON.parse(blobData).datas
-                }
-                Notify.create({
-                    message: message,
-                    type: 'negative',
-                    textColor:'white',
-                    position: 'top',
-                    closeBtn: true,
-                    timeout: 0,
-                    classes: "text-pre-wrap"
-                })
+          })
+    },
+    // Convert blob to text
+    BlobReader: function(data) {
+      const fileReader = new FileReader();
+      return new Promise((resolve, reject) => {
+        fileReader.onerror = () => {
+          fileReader.abort()
+          reject(new Error('Problem parsing blob'));
+        }
+        fileReader.onload = () => {
+          resolve(fileReader.result)
+        }
+        fileReader.readAsText(data)
+      })
+    },
+    generateReport: function() {
+      AuditService.generateAuditReport(this.auditId)
+          .then(response => {
+            var blob = new Blob([response.data], {type: "application/octet-stream"});
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = response.headers['content-disposition'].split('"')[1];
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+          })
+          .catch( async err => {
+            var message = "Error generating template"
+            if (err.response && err.response.data) {
+              var blob = new Blob([err.response.data], {type: "application/json"})
+              var blobData = await this.BlobReader(blob)
+              message = JSON.parse(blobData).datas
+            }
+            Notify.create({
+              message: message,
+              type: 'negative',
+              textColor:'white',
+              position: 'top',
+              closeBtn: true,
+              timeout: 0,
+              classes: "text-pre-wrap"
             })
-        },
-		}
+          })
+    },
+  }
 }
 </script>
 
@@ -411,7 +392,6 @@ export default {
   /*margin-left: 0px; Cancel q-col-gutter-md for left*/
   /*margin-right: 16px; Cancel q-col-gutter-md for right*/
 }
-
 .edit-breadcrumb {
   position: fixed;
   top: 50px;
@@ -419,30 +399,24 @@ export default {
   left: 300px;
   z-index: 1;
 }
-
 .q-menu > .q-item--active {
   color: white;
   background-color: $blue-14;
 }
-
 .card-screenshots {
   height: calc(100vh - 120px); /* 100% Full-height */
   overflow-x: hidden; /* Disable horizontal scroll */
   margin-right: 16px;
 }
-
 .affix {
   width: calc(16.6667% - 69px);
 }
-
 .caption-text input {
   text-align: center;
 }
-
 .multi-colors-bar {
   height: 5px;
 }
-
 .drawer-footer {
 // left: 0!important;
 // height: 30%;
@@ -450,9 +424,7 @@ export default {
   color: black;
   font-size: 12px;
 }
-
 .edit-drawer {
-	// height: 70%;
-
+// height: 70%;
 }
 </style>
